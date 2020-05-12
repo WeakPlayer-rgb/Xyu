c50ab32425e248a80ce663dccf217b8241ffd3aa
WeakPlayer-rgb <62828763+WeakPlayer-rgb@users.noreply.github.com>
2020-05-12 16:08:15 +0500

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -4,27 +4,27 @@ import requests
 class VkBot:
 
     def __init__(self, user_id):
-        print("\n"+"Я родился")
+        print('\n'+'Я родился')
         self._USER_ID = user_id
         self._USERNAME = self._get_user_name_from_vk_id(user_id)
 
-        self._COMMANDS = ["ПРИВЕТ","ЗАКАЗ"]
+        self._COMMANDS = ['ПРИВЕТ','ЗАКАЗ']
 
     def _get_user_name_from_vk_id(self,user_id):
-        request  = requests.get("https://vk.com/id"+str(user_id))
+        request  = requests.get('https://vk.com/id'+str(user_id))
         bs = bs4.BeautifulSoup(request.text,'html.parser')
 
-        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
+        user_name = self._clean_all_tag_from_str(bs.findAll('title')[0])
 
         return user_name.split()[0]
 
     def new_message(self,message):
         if message.upper() == self._COMMANDS[0]:
-            return f"Дарова, {self._USERNAME}!"
+            return f'Дарова, {self._USERNAME}!'
         elif message.upper() == self._COMMANDS[1]:
             return self.search_sportmaster()
         else:
-            return "Хуй"
+            return 'Хуй'
 
     def search_sportmaster(self):
         print("Ну допустим")
