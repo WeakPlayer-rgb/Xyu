3e366d218bc92c90ecec9b2973a2f0ecf4b944e3
WeakPlayer-rgb <62828763+WeakPlayer-rgb@users.noreply.github.com>
2020-05-13 15:18:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
             
+            print('New message:')
+            print(f'For me by: {event.user_id}', end='')
             
             bot = VkBot(event.user_id)
             write_msg(event.user_id, bot.new_message(event.text))
             
+            print('Text: ', event.text)
