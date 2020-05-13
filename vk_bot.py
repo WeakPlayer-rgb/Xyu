83eb512f994a6411bdc7e57da49b222a900b658d
WeakPlayer-rgb <62828763+WeakPlayer-rgb@users.noreply.github.com>
2020-05-13 15:18:01 +0500

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -24,7 +24,6 @@ class VkBot:
         elif message.upper() == self._COMMANDS[1]:
             return self.search_sportmaster()
         else:
-            return 'Хуй'
 
     def search_sportmaster(self):
         print("Ну допустим")
